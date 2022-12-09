22ea6528b6631f8f5d5ddd61761f04d6939da597
Henry Kondratko <henry.kondratko@gmail.com>
2022-12-09 10:42:14 -0500

    weather component styling

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,20 +1,40 @@
 import React from 'react';
-import {Card} from "semantic-ui-react";
+import '../styles.css';
 import moment from 'moment';
+import { Button } from 'semantic-ui-react';
 
-const CardExampleCard = ({weatherData}) => (
-    <Card>
-        <Card.Content>
-            <Card.Header className="header">{weatherData.name}</Card.Header>
-            <p>Temperature: {weatherData.main.temp} &deg;F</p>
-            <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
-            <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
-            <p>Description: {weatherData.weather[0].description}</p>
-            <p>Humidity: {weatherData.main.humidity}%</p>
-            <p>Day: {moment().format('dddd')}</p>
-            <p>Date: {moment().format('LL')}</p>
-        </Card.Content>
-    </Card>
+const refresh = () => {
+    window.location.reload();
+}
+
+const WeatherCard = ({weatherData}) => (
+    <div className="main">
+        <div className="top">
+            <p className="header">{weatherData.name}</p>
+            <Button className="button"
+                    inverted color="blue"
+                    circular icon='refresh'
+                    onClick={refresh}
+            />
+        </div>
+
+        <div className="flex">
+            <p className="day">Day: {moment().format('dddd')}</p>
+            <p className="day">{moment().format('LL')}</p>
+            <p className="description">{weatherData.weather[0].main}</p>
+        </div>
+
+        <div className="flex">
+            <p className="temp">Temperature: {weatherData.main.temp}&deg;F</p>
+            <p className="temp">Humidity: {weatherData.main.humidity}%</p>
+        </div>
+
+        <div className="flex">
+            <p className="sunrise-sunset">Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+            <p className="sunrise-sunset">Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+        </div>
+
+    </div>
 )
 
-export default CardExampleCard;
+export default WeatherCard;
