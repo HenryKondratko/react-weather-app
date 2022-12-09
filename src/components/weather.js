dfccacc2fb843987b37971431ef5996fec973af5
Henry Kondratko <henry.kondratko@gmail.com>
2022-12-09 10:11:13 -0500

    added formatting for weather data, added humidity

--- src/components/weather.js
+++ src/components/weather.js
@@ -5,10 +5,11 @@ const CardExampleCard = ({weatherData}) => (
     <Card>
         <Card.Content>
             <Card.Header className="header">{weatherData.name}</Card.Header>
-            <p>Temperature: {weatherData.main.temp}</p>
-            <p>Sunrise: {weatherData.sys.sunrise}</p>
-            <p>Sunset: {weatherData.sys.sunset}</p>
+            <p>Temperature: {weatherData.main.temp} &deg;F</p>
+            <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+            <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
             <p>Description: {weatherData.weather[0].description}</p>
+            <p>Humidity: {weatherData.main.humidity}%</p>
         </Card.Content>
     </Card>
 )
