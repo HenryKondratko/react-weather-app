0fa43b2c300922d1b9a156313093bdcb6188a078
Henry Kondratko <henry.kondratko@gmail.com>
2022-12-09 11:24:55 -0500

    added loader to page

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles.css';
 import moment from 'moment';
-import { Button } from 'semantic-ui-react';
+import {Button} from 'semantic-ui-react';
 
 const refresh = () => {
     window.location.reload();
