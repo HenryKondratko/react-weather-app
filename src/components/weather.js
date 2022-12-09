24ec8fb8fe1939a571f02a5876cc82f92ceb0aec
Henry Kondratko <henry.kondratko@gmail.com>
2022-12-09 10:18:57 -0500

    added day and date to weather component

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Card} from "semantic-ui-react";
+import moment from 'moment';
 
 const CardExampleCard = ({weatherData}) => (
     <Card>
@@ -10,6 +11,8 @@ const CardExampleCard = ({weatherData}) => (
             <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
             <p>Description: {weatherData.weather[0].description}</p>
             <p>Humidity: {weatherData.main.humidity}%</p>
+            <p>Day: {moment().format('dddd')}</p>
+            <p>Date: {moment().format('LL')}</p>
         </Card.Content>
     </Card>
 )
